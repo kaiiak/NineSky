9be86a6e9fbe75f592e8e5d35efcad00845da388
Yoock <itkaikai@gmail.com>
2015-10-21 05:36:43 -0800

    add server.py

---
+++ server.py
@@ -0,0 +1,14 @@
+#-*-coding:UTF8-*-
+#!/usr/bin/env python
+
+#用Flask库来实现一个简单的web server
+
+from flask import Flask
+app = Flask(name)
+
+@app.route("/")
+def hello():
+	return  "Hello World!"
+
+if __name___ == "__main__":
+	app.run()
